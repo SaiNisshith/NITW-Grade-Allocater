905fca76070009df7e6d6ffae62d75864c317c05
SaiNisshith <108432633+SaiNisshith@users.noreply.github.com>
2022-07-13 01:18:45 +0530

    Final version

--- NITW_Grade_Assigner.cpp
+++ NITW_Grade_Assigner.cpp
@@ -15,11 +15,12 @@ int main(){
     int nofs;
     cin>>nofs;
     Student* s= new Student[nofs+1];
-     cout<<"Enter the minimum percentage for Passing (35 or 40):";
+     cout<<endl<<"Enter the minimum percentage for Passing (35 or 40):";
     double mi;
     cin>>mi;
-    cout<<"If a Student is ABSENT for END Exam, while entering END marks please enter -1 and for the rest exams enter 0"<<endl;
-    cout<<"Provide the data in the format of (ROLL Number - Internal Marks - Mid - End) "<<endl;
+    cout<<endl;
+    cout<<endl<<"If a Student is ABSENT for END Exam, for END marks please enter -1 and for the rest exams 0"<<endl;
+    cout<<endl<<"Provide the data in the format of (ROLL Number - Internal Marks - Mid - End) "<<endl;
         double numsq = 0;
         double sum=0;
         int count=0;
@@ -44,10 +45,10 @@ int main(){
         cout<<"\tStandard Deviation: "<<standev<<endl;
         // cout<<mean<<" "<<standev<<endl;
     double scri;
-     cout<<"For Giving S grade, choose the factor for Standard Deviation(make sure it is between 1.5 - 2)"<<endl;
+     cout<<endl<<"For Giving S grade, choose the factor for Standard Deviation(make sure it is between 1.5 - 2): ";
     cin>>scri;
     double subs;
-     cout<<"For the Subsequent Grades , what SD factor you would like to choose (choose between 0.5 - 1)"<<endl;
+     cout<<endl<<"For the Subsequent Grades , what SD factor you would like to choose (choose between 0.5 - 1): ";
     cin>>subs;
     double ss=mean + (standev*scri);
     double as = mean + (standev*(scri-subs));
@@ -82,12 +83,12 @@ int main(){
     }
     ofstream f;
      f.open("result.txt");
-    cout<<"The output will be  (Roll - Total Marks - Grade Allotated)";
+    cout<<endl<<"The output will be  (Roll - Total Marks - Grade Allotated)";
     for(int i=1; i<=nofs; i++){
         cout<<"\t"<<s[i].roll<<" - "<<s[i].total<<" - "<<s[i].grade<<endl;
         f<<s[i].roll<<" - "<<s[i].total<<" - "<<s[i].grade<<endl;
     }
      f.close();
-     cout<<"Check the folder, from where this cpp file was opened. Now, you will be able to get one more txt file where every assigned Grade is stored in the order(ROLL NO - TOTAL MARKS - ASSIGNED GRADE)"<<endl;
-    cout<<" Thank you for using ....";
+     cout<<endl<<endl<<"Check the folder, from where this cpp file was opened. Now, you will be able to get one more txt file where every assigned Grade is stored in the order(ROLL NO - TOTAL MARKS - ASSIGNED GRADE)"<<endl;
+    cout<<endl<<endl<<endl<<" Thank you for using ....";
 }
